4be5e5747359dd63ed1c306c5cf4ca438b209d4b
SparXFusion <s2234nadar@gmail.com>
2021-11-19 22:43:05 +0530

    FusionReactor: drop useless categories
    * Also introduce interface category
    
    Signed-off-by: SparXFusion <s2234nadar@gmail.com>

--- src/com/fusion/reactor/fragments/Interface.java
+++ src/com/fusion/reactor/fragments/Interface.java
@@ -2,48 +2,38 @@ package com.fusion.reactor.fragments;
 
 import com.android.internal.logging.nano.MetricsProto;
 
-import android.os.Bundle;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
-import android.os.UserHandle;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Context;
 import android.content.ContentResolver;
+import android.content.DialogInterface;
 import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.SystemProperties;
+import android.os.UserHandle;
 import androidx.preference.ListPreference;
 import androidx.preference.Preference;
-import androidx.preference.PreferenceGroup;
 import androidx.preference.PreferenceScreen;
-import androidx.preference.PreferenceCategory;
 import androidx.preference.Preference.OnPreferenceChangeListener;
-import androidx.preference.PreferenceFragment;
 import androidx.preference.SwitchPreference;
 import android.provider.Settings;
 import com.android.settings.R;
 
-import java.util.Locale;
-import android.text.TextUtils;
-import android.view.View;
+import java.util.Arrays;
+import java.util.HashSet;
 
 import com.android.settings.SettingsPreferenceFragment;
-import com.android.settings.Utils;
-import android.util.Log;
 
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.HashMap;
-import java.util.Collections;
-
-public class StatusBarSettings extends SettingsPreferenceFragment implements
+public class Interface extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
 
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
 
-        addPreferencesFromResource(R.xml.fusion_reactor_statusbar);
-
-        PreferenceScreen prefSet = getPreferenceScreen();
+        addPreferencesFromResource(R.xml.fusion_reactor_interface);
 
     }
 
@@ -57,5 +47,4 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements
     public int getMetricsCategory() {
         return MetricsProto.MetricsEvent.FUSION;
     }
-
 }
